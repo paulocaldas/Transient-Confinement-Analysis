9a86033a41b94cfe55280e40da5fe443041be022
Paulo Caldas <paulocaldas90@gmail.com>
2021-10-19 17:22:47 +0100

    added area and conf_counts as output

--- bkg_func/main.py
+++ bkg_func/main.py
@@ -9,7 +9,8 @@ import numpy as np
 from bkg_func.utils import read_trackmate_xml_tracks, FilterTracks, SingleTrack
 from bkg_func.conf_ratio_func import ComputeConfinementRatioScore, FindTransitionPoints, DenoiseTransitionPoints
 from bkg_func.conf_ratio_func import PlotConfinedRegions, ComputeSubSegmentStats, ShowStats
-from bkg_func.multiple_track_func import AnalyzeAllTracks, ExtractConfinementLifetimes, PlotLifetimes, TrimEnds
+from bkg_func.multiple_track_func import AnalyzeAllTracks, ExtractConfinementLifetimes, ExtractConfinementCounts, ExtractConfinementCountsII
+from bkg_func.multiple_track_func import PlotLifetimes, PlotConfinementCounts, TrimEnds, ExtractConfinementArea
 
 
 def ReadTracks(file, minlen):
@@ -19,7 +20,7 @@ def ReadTracks(file, minlen):
     # apply filter for minimum lenght
     table = FilterTracks(table, minlen)
     n_tracks = table.TRACK_ID.unique().max() 
-    print('{:} tracks (filter = {} frames)'.format(int(n_tracks), minlen))    
+    print('{:} tracks (filter > {} frames)'.format(int(n_tracks), minlen))    
 
     return table, rate, n_tracks
 
@@ -41,19 +42,37 @@ def TrajectoryClassificationAllTracks(file, min_track_len = 30, tracks = 30, win
     
     all_tracks, frame_rate, n_tracks = ReadTracks(file, minlen = min_track_len)
     
-    print('confinement threshold: {} '.format(p_thres))
-    print('time threshold: {} frames ~ {}s'.format(np.ceil(t_thres / frame_rate), t_thres))
+    print('confinement threshold: {} ; time threshold: {} frames ~ {}s'.format(p_thres, np.ceil(t_thres / frame_rate), t_thres))
     
     all_tracks_stats = AnalyzeAllTracks(all_tracks, frame_rate, tracks = tracks, w = window, p_thres = p_thres, t_thres = t_thres)
     
     # discard all confined motions identified at the begnning and end of each track
     if trim_trajectory_ends == True:
         all_tracks_stats = TrimEnds(all_tracks_stats)
-        
-    lifetimes = ExtractConfinementLifetimes(all_tracks_stats)
-    PlotLifetimes(lifetimes)
     
+    # extract and plot lifetimes confined vs. free diffusion
+    lifetimes = ExtractConfinementLifetimes(all_tracks_stats)
+      
     # save lifetimes table in the output folder
     lifetimes.to_csv('outputs/Lifetimes_ConfinedMotion{}.csv'.format(file.split('/')[-1][:-4]), index = False)
     
-    return lifetimes
+    # extract number of tracks with confinement events
+    conf_counts = ExtractConfinementCountsII(all_tracks_stats) 
+    conf_counts_total = sum(conf_counts.values())
+    
+    # extract average cage area of the confined tracks
+    cage_area = ExtractConfinementArea(all_tracks_stats)['confined'].mean()
+    
+    # print mesages
+    print('avg cage area: {0:.3} nm2 ; tracks w confinement: {1}/{2} ~ {3:.3}% '.format(cage_area,
+                                                              conf_counts_total, len(all_tracks_stats),
+                                                              (conf_counts_total/len(all_tracks_stats)*100)))
+    
+    #print('tracks w confinement: {}/{} ~ {:.3}% '.format(conf_counts_total, len(all_tracks_stats), 
+    #                                          (conf_counts_total/len(all_tracks_stats)*100)))
+    
+    # plot lifetimes and number of confinement events
+    PlotLifetimes(lifetimes)
+    PlotConfinementCounts(conf_counts)
+    
+    return lifetimes, conf_counts , cage_area
--- bkg_func/multiple_track_func.py
+++ bkg_func/multiple_track_func.py
@@ -97,25 +97,94 @@ def ExtractConfinementCounts(all_tracks_stats):
                                               (tracks_w_confinement/len(all_tracks_stats)*100)))
     return tracks_w_confinement
 
+def ExtractConfinementCountsII(all_tracks_stats):
+    '''returns a distrbuiton of confined motion '''
+    
+    conf_dist = {'one':0, 'two':0, '>three':0}
+    
+    tracks_wo_confinement = 0
+    
+    for track in all_tracks_stats:
+
+        if 'confined' in track['mode'].values:
+            
+            if track['mode'].value_counts()['confined'] == 1:
+                conf_dist['one'] += 1
+            
+            if track['mode'].value_counts()['confined'] == 2:
+                conf_dist['two'] += 1
+                
+            if track['mode'].value_counts()['confined'] >= 3:
+                conf_dist['>three'] += 1
+        
+        if 'confined' not in track['mode'].values: 
+            tracks_wo_confinement += 1
+        
+    return conf_dist
+
+def ExtractConfinementArea(all_tracks_stats):
+    '''takes the output from the function AnalyzeAllTracks and returns
+    a dataframe with the cage area of each subsegment analyzed'''
+    
+    tracks_area_confined = []
+    tracks_area_free = []
+
+    for track in all_tracks_stats:
+
+        confined_areas = track[track['mode'] == 'confined']['cage_area (nm2)'].values
+        free_areas = track[track['mode'] == 'free_diff']['cage_area (nm2)'].values
+
+        tracks_area_confined.append(confined_areas)
+        tracks_area_free.append(free_areas)
+
+    # unpack lists
+    tracks_area_confined = [i for j in tracks_area_confined for i in j]
+    tracks_area_free = [i for j in tracks_area_free for i in j]
+    
+    # make a dataframe
+    
+    tracks_area = pd.DataFrame([tracks_area_confined, tracks_area_free], index = ['confined','free']).T
+    
+    return tracks_area
+
 def PlotLifetimes(lifetimes, colors = ['steelblue', 'crimson']):
     '''takes the output from the ExtractConfinementLifetimes to 
     plot confinement vs. free diffusion lifetimes'''
     
-    fig, ax = plt.subplots(figsize = (1.5,2), dpi = 150)
+    fig, ax1 = plt.subplots(figsize = (2,2.5), dpi = 150)
     
     sns.barplot(data = lifetimes,
-                ax = ax, palette = colors, edgecolor = 'black', 
-            linewidth = 1, errwidth=1, capsize=0.1)
+                ax = ax1, palette = colors, edgecolor = 'black', 
+                linewidth = 1, errwidth=1, capsize=0.1)
     
     # fancy stuff
-    ax.set_ylabel('subdiffusion time (s)', fontsize = 9); #ax.set_xlabel('type of motion', fontsize = 11);
+    ax1.set_ylabel('subdiffusion time (s)', fontsize = 9); #ax.set_xlabel('type of motion', fontsize = 11);
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['bottom'].set_visible(True)
+    ax1.tick_params(direction = 'out', top=False, right = False, bottom = False, labelsize=8)
+    
+    #ax.set_ylim([0, DefineYmax(ax)])
+
+def PlotConfinementCounts(conf_counts, colors = ['peachpuff', 'salmon', 'orange']):
+    
+    data_to_plot = pd.DataFrame.from_dict(conf_counts, orient = 'index').T
+    
+    fig, ax = plt.subplots(figsize = (2,1.5), dpi = 150)
+    
+    sns.barplot(data = data_to_plot, ax = ax, palette = colors, edgecolor = 'black', 
+                    linewidth = 1, errwidth=1, capsize=0.1,)
+    
+    # fancy stuff
+    ax.set_ylabel('number of tracks', fontsize = 9); ax.set_xlabel('number of confined events', fontsize = 8);
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(True)
     ax.tick_params(direction = 'out', top=False, right = False, bottom = False, labelsize=8)
     
    # ax.set_ylim([0, DefineYmax(ax)])
-
+    
+    return conf_counts
 
 def TrimEnds(all_tracks_stats, start = True, end = True):
     '''discards confined motions identified at the end or start of each trajectory
